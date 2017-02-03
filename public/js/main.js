09ac5ed0a32203edcd5456674809225f755b4aa8
Wes Hediger <wes.hediger@gmail.com>
2017-02-03 15:37:04 -0700

    refactor js and add faqs js, faqs model

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,61 @@
+window.onload = function(){
+    document.getElementById('faqs').addEventListener("click", showFaqs);
+    document.getElementById('closeFaq').addEventListener("click", hideFaqs);
+    document.getElementById('faqModel').addEventListener("click", hideFaqs);
+    document.getElementById('events').addEventListener("click", showEvents);
+    document.getElementById('closeCal').addEventListener("click", hideEvents);
+    document.getElementById('calModel').addEventListener("click", hideEvents);
+}
 
+function hideInfo() {
+  var video = document.getElementById('video');
+  video.pause();
+  var sht = document.getElementById('short');
+  sht.className = "hide";
+  var hideHome = document.getElementsByClassName('info');
+  for (var i = 0; i < hideHome.length; i++) {
+    hideHome[i].className = "info hide";
+  }
+}
+
+function showInfo() {
+  setTimeout(function(){
+    var video = document.getElementById('video');
+    video.play();
+    var sht = document.getElementById('short');
+    sht.className = "show";
+  }, 1000);
+  var hideHome = document.getElementsByClassName('info');
+  for (var i = 0; i < hideHome.length; i++) {
+    hideHome[i].className = "info show";
+  }
+}
+
+
+function showEvents() {
+  var cal = document.getElementById('calModel');
+  cal.className = "model-shield show";
+  hideInfo();
+}
+
+function hideEvents() {
+  var cal = document.getElementById('calModel');
+  cal.className = "model-shield hide";
+  showInfo();
+}
+
+function showFaqs() {
+  var faq = document.getElementById('faqModel');
+  faq.className = "model-shield show";
+  hideInfo();
+}
+
+function hideFaqs() {
+  var faq = document.getElementById('faqModel');
+  faq.className = "model-shield hide";
+}
+
+// js for mobile burger
 function burgerFunction(x) {
     x.classList.toggle("change");
     var b = document.getElementsByClassName('mobileBurger');
