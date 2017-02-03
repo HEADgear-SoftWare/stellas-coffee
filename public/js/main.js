e4aeb1c0fc48124c734e37b5944126dca775ee24
Wes Hediger <wes.hediger@gmail.com>
2017-02-02 19:17:01 -0700

    add mobile menue animation

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,6 @@ function burgerFunction(x) {
     x.classList.toggle("change");
     var b = document.getElementsByClassName('mobileBurger');
     b[0].classList.toggle("flipFlop");
+    var c = document.getElementsByClassName('header-nav');
+    c[0].classList.toggle("header-nav-close");
 }
