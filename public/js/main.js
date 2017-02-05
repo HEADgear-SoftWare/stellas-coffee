8c4bf53fd9f2c6a93b5c716172491fd3855f8944
Wes Hediger <wes.hediger@gmail.com>
2017-02-05 12:29:09 -0700

    add video fadein transistion on canplay

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 window.onload = function(){
   var screenWidth = document.documentElement.clientWidth;
   if(screenWidth > 750) {
-    var video = '<video media="screen and (min-width: 750px)" preload="preload" id="video" autoplay="autoplay" loop="loop"><source src="assets/Videos/couple-drinking-coffee.mp4" type="video/mp4"></source></video>';
+    var video = '<video media="screen and (min-width: 750px)" preload="preload" id="video" loop="loop"><source src="assets/Videos/couple-drinking-coffee.mp4" type="video/mp4"></source></video>';
     document.body.innerHTML += video;
   }
+
   document.getElementById('faqs').addEventListener("click", showFaqs);
   document.getElementById('closeFaqs').addEventListener("click", hideFaqs);
   document.getElementById('faqModel').addEventListener("click", hideFaqs);
@@ -13,8 +14,20 @@ window.onload = function(){
   document.getElementById('events').addEventListener("click", showEvents);
   document.getElementById('closeCal').addEventListener("click", hideEvents);
   document.getElementById('calModel').addEventListener("click", hideEvents);
+
+// fadein video transition +===}========>
+  var video = document.getElementById('video');
+  video.oncanplay = function(){
+    video.className = "fadeIn";
+    setTimeout(function() {
+      video.play();
+    }, 300);
+  }
 }
 
+// page scripts +===}========>
+
+// pause video after 2 min to release client resources
 setTimeout(function() {
   document.getElementById('video').pause();
 }, 120000);
